5c5b0a828f9a97a9c86f097f55b5daf690f7416f
Max Desiatov <max@desiatov.com>
2018-11-18 19:50:37 +0000

    Add API for filtering cells by rows and columns (#7)
    There are APIs to filter cells by rows and columns separately, but no API for filtering by both criteria at the same time. This is added with an implementation similar to those previous APIs with new assertions in tests.
    
    * Add API for filtering cells by rows and columns
    * Cleanup DerivedData as an attempt to fix coverage

--- Sources/CoreXLSX/Worksheet.swift
+++ Sources/CoreXLSX/Worksheet.swift
@@ -32,21 +32,33 @@ public struct Worksheet: Codable {
     case mergeCells
   }
 
-  /// Return all cells that are contained in a given worksheet and set of
+  /// Return all cells that are contained in a given worksheet and collection of
   /// columns.
-  public func cells<T>(atColumns: T) -> [Cell]
+  public func cells<T>(atColumns columns: T) -> [Cell]
   where T: Collection, T.Element == ColumnReference {
     return sheetData.rows.map {
-      return $0.cells.filter { atColumns.contains($0.reference.column) }
+      return $0.cells.filter { columns.contains($0.reference.column) }
     }
     .reduce([]) { $0 + $1 }
   }
 
-  /// Return all cells that are contained in a given worksheet and set of rows.
+  /// Return all cells that are contained in a given worksheet and collection of
+  /// rows.
   public func cells<T>(atRows rows: T) -> [Cell]
   where T: Collection, T.Element == UInt {
     return sheetData.rows.filter { rows.contains($0.reference) }
       .reduce([]) { $0 + $1.cells }
+  }
+
+  /// Return all cells that are contained in a given worksheet and collections
+  /// of rows and columns.
+  public func cells<T1, T2>(atColumns columns: T1, rows: T2) -> [Cell]
+    where T1: Collection, T1.Element == ColumnReference,
+    T2: Collection, T2.Element == UInt {
+      return sheetData.rows.filter { rows.contains($0.reference) }.map {
+        return $0.cells.filter { columns.contains($0.reference.column) }
+      }
+      .reduce([]) { $0 + $1 }
   }
 }
 
