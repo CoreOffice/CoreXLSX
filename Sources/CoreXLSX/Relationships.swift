e7bc09959532ebbccbf7c97722d152d4fae4df17
Max Desiatov <max@desiatov.com>
2018-12-03 10:47:53 +0000

    Make Relationships public, add parseRelationships (#22)
    As reported in #19 without a `public Relationships` type `Workbook` values can't be matched to `Worksheet` cleanly. `Relationships` type already existed in the library, so it makes sense to make it `public` and to expose `public func parseRelationships` that returns a value of that type for a given `XLSXFile` instance.
    
    Resolves #19.

--- Sources/CoreXLSX/Relationships.swift
+++ Sources/CoreXLSX/Relationships.swift
@@ -5,16 +5,16 @@
 //  Created by Max Desiatov on 27/10/2018.
 //
 
-struct Relationships: Codable {
-  let items: [Relationship]
+public struct Relationships: Codable, Equatable {
+  public let items: [Relationship]
 
   enum CodingKeys: String, CodingKey {
     case items = "relationship"
   }
 }
 
-struct Relationship: Codable, Equatable {
-  enum SchemaType: String, Codable {
+public struct Relationship: Codable, Equatable {
+  public enum SchemaType: String, Codable {
     case officeDocument = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument"
     case extendedProperties = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/extended-properties"
     case coreProperties = "http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties"
@@ -24,7 +24,7 @@ struct Relationship: Codable, Equatable {
     case theme = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/theme"
   }
 
-  let id: String
-  let type: SchemaType
-  let target: String
+  public let id: String
+  public let type: SchemaType
+  public let target: String
 }
