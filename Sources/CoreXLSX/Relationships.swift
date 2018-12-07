0e1b8baaa0149be1c95a758231d84de24b1b80e9
Max Desiatov <max@desiatov.com>
2018-12-07 11:26:04 +0000

    Add `case calcChain` to `Relationship.SchemaType`

--- Sources/CoreXLSX/Relationships.swift
+++ Sources/CoreXLSX/Relationships.swift
@@ -15,6 +15,7 @@ public struct Relationships: Codable, Equatable {
 
 public struct Relationship: Codable, Equatable {
   public enum SchemaType: String, Codable {
+    case calcChain = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/calcChain"
     case officeDocument = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument"
     case extendedProperties = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/extended-properties"
     case coreProperties = "http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties"
