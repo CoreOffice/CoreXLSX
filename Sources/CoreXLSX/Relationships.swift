f02d7f410c8c9a607309829fffcddb4b0628d93a
Max Desiatov <max@desiatov.com>
2018-12-11 14:12:49 +0000

    Add more cases to `Relationship.SchemaType` (#25)
    This addresses parts of #24
    
    * Add `case calcChain` to `Relationship.SchemaType`
    * Add `pivotCache` to `Relationships.SchemaType`

--- Sources/CoreXLSX/Relationships.swift
+++ Sources/CoreXLSX/Relationships.swift
@@ -15,6 +15,7 @@ public struct Relationships: Codable, Equatable {
 
 public struct Relationship: Codable, Equatable {
   public enum SchemaType: String, Codable {
+    case calcChain = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/calcChain"
     case officeDocument = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument"
     case extendedProperties = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/extended-properties"
     case coreProperties = "http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties"
@@ -22,6 +23,7 @@ public struct Relationship: Codable, Equatable {
     case sharedStrings = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/sharedStrings"
     case styles = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles"
     case theme = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/theme"
+    case pivotCache = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/pivotCacheDefinition"
   }
 
   public let id: String
