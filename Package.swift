5bbc3eafbc3718729a88503c39d56ad102ae0310
Max Desiatov <max@desiatov.com>
2019-04-09 08:23:04 +0100

    Bump XMLCoder dependency to 0.4.0, update .travis

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),
     .package(url: "https://github.com/maxdesiatov/XMLCoder.git",
-             .upToNextMajor(from: "0.3.1")),
+             .upToNextMajor(from: "0.4.0")),
     .package(url: "https://github.com/weichsel/ZIPFoundation.git",
              .upToNextMajor(from: "0.9.8")),
   ],
